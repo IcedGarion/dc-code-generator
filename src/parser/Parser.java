40d15384c3b11775b333a8ff1726ded3542a6eca
Garion <20010613@studenti.uniupo.it>
2017-03-15 23:18:30 +0100

    testate funzioni lhs-rhs, costruzione grammatica e funzioni dei nonTerminali

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,7 +1,6 @@
 package parser;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,33 +13,34 @@ public class Parser
 {
 	private Scanner scanner;
 	private Token currentToken;
-	private String grammarPath = "./resources/grammar";
-
-	//lista di stringhe che sono tutte le regole della grammatica
-	private ArrayList<String> prod;
+	private String defaultGrammarPath = "./resources/grammar";
+	private ArrayList<String> grammar; 	//lista di stringhe che sono tutte le regole della grammatica
+										//formato: un nonTerm seguito da uno o piu term/non term (solo spazi)
 	
 	public Parser(Scanner s) throws IOException
 	{
 		this.scanner = s;
-		prodFill(grammarPath);
+		grammar = new ArrayList<String>();
+		grammarFill(defaultGrammarPath);
 	}
 	
-	private void prodFill(String Path) throws IOException
+	private void grammarFill(String Path) throws IOException
 	{
 		BufferedReader t = new BufferedReader(new FileReader(Path));
 		String line = t.readLine();
 		
 		while(line != null)
 		{
-			prod.add(line);
+			grammar.add(line);
 			line = t.readLine();
 		}
 		
+		t.close();
 	}
 	
-	public ArrayList<String> grammar()
+	public ArrayList<String> getGrammar()
 	{
-		return prod;
+		return grammar;
 	}
 	
 	private void match(TokenType type) throws SyntacticException, LexicalException, IOException
@@ -63,10 +63,10 @@ public class Parser
 		ArrayList<String> startsWithNT = new ArrayList<String>();
 		
 		//agginge a una lista le regole che hanno come LHS questo non Term (prog)
-		for(int i=0; i<prod.size(); i++)
+		for(int i=0; i<grammar.size(); i++)
 		{
-			if(prod.get(i).startsWith(nonTerm))
-				startsWithNT.add(prod.get(i));
+			if(grammar.get(i).startsWith(nonTerm))
+				startsWithNT.add(grammar.get(i));
 		}
 		
 		//prog ha una sola regola: prog->Dcls Stms eof
@@ -90,10 +90,10 @@ public class Parser
 		ArrayList<String> startsWithNT = new ArrayList<String>();
 		
 		//agginge a una lista le regole che hanno come LHS questo non Term (prog)
-		for(int i=0; i<prod.size(); i++)
+		for(int i=0; i<grammar.size(); i++)
 		{
-			if(prod.get(i).startsWith(nonTerm))
-				startsWithNT.add(prod.get(i));
+			if(grammar.get(i).startsWith(nonTerm))
+				startsWithNT.add(grammar.get(i));
 		}
 		
 		//Dcls ha 2 regole: Dcls->Dcl Dcls
@@ -119,10 +119,10 @@ public class Parser
 		ArrayList<String> startsWithNT = new ArrayList<String>();
 		
 		//agginge a una lista le regole che hanno come LHS questo non Term (prog)
-		for(int i=0; i<prod.size(); i++)
+		for(int i=0; i<grammar.size(); i++)
 		{
-			if(prod.get(i).startsWith(nonTerm))
-				startsWithNT.add(prod.get(i));
+			if(grammar.get(i).startsWith(nonTerm))
+				startsWithNT.add(grammar.get(i));
 		}
 		
 		// Dcl ha 2 regole: Dcl->floatdcl id
@@ -142,21 +142,132 @@ public class Parser
 		
 		return true;
 	}
-	private boolean parseStms()
+	private boolean parseStms() throws LexicalException, IOException, SyntacticException
 	{
-		return false;
+		String nonTerm = "Stms";
+		Token nxt = scanner.peekToken();
+		ArrayList<String> startsWithNT = new ArrayList<String>();
+		
+		//agginge a una lista le regole che hanno come LHS questo non Term
+		for(int i=0; i<grammar.size(); i++)
+		{
+			if(grammar.get(i).startsWith(nonTerm))
+				startsWithNT.add(grammar.get(i));
+		}
+		
+		// Dcl ha 2 regole: stms->stm stms
+		if(predict(nonTerm, startsWithNT.get(0)).contains(nxt.getType()))
+		{
+			parseStm();
+			parseStms();
+		}
+		//stms->eps
+		else if(predict(nonTerm, startsWithNT.get(1)).contains(nxt.getType()))
+		{
+			//EPS
+		}
+		else
+			throw new SyntacticException();
+		
+		return true;
 	}
-	private boolean parseStm()
+	private boolean parseStm() throws LexicalException, IOException, SyntacticException
 	{
-		return false;
+		String nonTerm = "Stm";
+		Token nxt = scanner.peekToken();
+		ArrayList<String> startsWithNT = new ArrayList<String>();
+		
+		//agginge a una lista le regole che hanno come LHS questo non Term
+		for(int i=0; i<grammar.size(); i++)
+		{
+			if(grammar.get(i).startsWith(nonTerm))
+				startsWithNT.add(grammar.get(i));
+		}
+		
+		// Dcl ha 2 regole: stm->id assign val expr
+		if(predict(nonTerm, startsWithNT.get(0)).contains(nxt.getType()))
+		{
+			match(TokenType.ID);
+			match(TokenType.ASSIGN);
+			parseVal();
+			parseExpr();
+		}
+		//stm->print id
+		else if(predict(nonTerm, startsWithNT.get(1)).contains(nxt.getType()))
+		{
+			match(TokenType.PRINT);
+			match(TokenType.ID);
+		}
+		else
+			throw new SyntacticException();
+		
+		return true;
 	}
-	private boolean parseExpr()
+	private boolean parseExpr() throws LexicalException, IOException, SyntacticException
 	{
-		return false;
+		String nonTerm = "Expr";
+		Token nxt = scanner.peekToken();
+		ArrayList<String> startsWithNT = new ArrayList<String>();
+		
+		//agginge a una lista le regole che hanno come LHS questo non Term
+		for(int i=0; i<grammar.size(); i++)
+		{
+			if(grammar.get(i).startsWith(nonTerm))
+				startsWithNT.add(grammar.get(i));
+		}
+		
+		// Dcl ha 3 regole: Expr->plus Val Expr
+		if(predict(nonTerm, startsWithNT.get(0)).contains(nxt.getType()))
+		{
+			match(TokenType.PLUS);
+			parseVal();
+			parseExpr();
+		}
+		//Expr->minus Val Expr
+		else if(predict(nonTerm, startsWithNT.get(1)).contains(nxt.getType()))
+		{
+			match(TokenType.MINUS);
+			parseVal();
+			parseExpr();
+		}
+		else if(predict(nonTerm, startsWithNT.get(2)).contains(nxt.getType()))
+		{
+			//EPS
+		}
+		else
+			throw new SyntacticException();
+		
+		return true;
 	}
-	private boolean parseVal()
+	
+	private boolean parseVal() throws LexicalException, IOException, SyntacticException
 	{
-		return false;
+		String nonTerm = "Val";
+		Token nxt = scanner.peekToken();
+		ArrayList<String> startsWithNT = new ArrayList<String>();
+		
+		//agginge a una lista le regole che hanno come LHS questo non Term
+		for(int i=0; i<grammar.size(); i++)
+		{
+			if(grammar.get(i).startsWith(nonTerm))
+				startsWithNT.add(grammar.get(i));
+		}
+		
+		// Dcl ha 3 regole: Val->inum
+		if(predict(nonTerm, startsWithNT.get(0)).contains(nxt.getType()))
+			match(TokenType.INUM);
+	
+		//Expr->minus Val Expr
+		else if(predict(nonTerm, startsWithNT.get(1)).contains(nxt.getType()))
+			match(TokenType.FNUM);
+
+		else if(predict(nonTerm, startsWithNT.get(2)).contains(nxt.getType()))
+			match(TokenType.ID);
+		
+		else
+			throw new SyntacticException();
+		
+		return true;
 	}
 	
 	private ArrayList<String> predict(String nT, String production)
@@ -164,13 +275,36 @@ public class Parser
 		return null;
 	}
 	
-	private String LHS(String production)
+	public String LHS(String production)
 	{
-		return null;
+		String ret = "";
+		int i = 0;
+		char c = production.charAt(i);
+		
+		while(c != ' ')
+		{
+			ret += c;
+			c = production.charAt(++i);
+		}
+		
+		return ret;
 	}
 	
-	private String RHS(String production)
+	public String RHS(String production)
 	{
-		return null;
+		String splitted[];
+		String ret = "";
+		
+		splitted = production.split(" ");
+		
+		for(int i=1; i<splitted.length; i++)
+		{
+			if(i != splitted.length -1)
+				ret += splitted[i] + " ";
+			else
+				ret += splitted[i];
+		}
+			
+		return ret;
 	}
 }
--- src/test/testParser.java
+++ src/test/testParser.java
@@ -14,8 +14,9 @@ import scanner.Scanner;
 
 public class testParser
 {
-	private String fileName = "./resources/testParse";
+	private String inputFileName = "./resources/testParse";
 	private String dummyFileName = "./resources/dummyParse";
+	private String grammarFileName = "./resources/grammar";
 	
 	@Before
 	public void writeFile() throws FileNotFoundException, UnsupportedEncodingException
@@ -23,30 +24,65 @@ public class testParser
 		//scrive file per i test
 		PrintWriter writer;
 
-		writer = new PrintWriter(fileName, "UTF-8");
+		writer = new PrintWriter(inputFileName, "UTF-8");
 		writer.write("f b\ni a\na = 5\nb = a + 3.2\np b\n");
 		writer.close();
 		
 		writer = new PrintWriter(dummyFileName, "UTF-8");
 		writer.write("f f\ni f\nb = a + ");
 		writer.close();
+		
+		writer = new PrintWriter(grammarFileName, "UTF-8");
+		writer.write("Prog Dcls Stms eof\nDcls Dcl Dcls\nDcls eps\nDcl floatdcl id\nDcl intdcl id\n"
+				+ "Stms Stm Stms\nStms eps\nStm id assign Val Expr\nStm print id\nExpr plus Val Expr\n"
+				+ "Expr minus Val Expr\nExpr eps\nVal inum\nVal fnum\nVal id\n");
+		writer.close();
 	}
+	
+	@Test
+	public void testHS() throws FileNotFoundException, IOException
+	{
+		Parser p = new Parser(new Scanner(inputFileName));
+		
+		assertEquals("Prog", p.LHS("Prog Dcls Stms eof"));
+		assertEquals("Dcls Stms eof", p.RHS("Prog Dcls Stms eof"));
+		assertEquals("Stms", p.LHS("Stms eps"));
+		assertEquals("eps", p.RHS("Stms eps"));
+		assertEquals("Stm", p.LHS("Stm id assign Val Expr"));
+		assertEquals("id assign Val Expr", p.RHS("Stm id assign Val Expr"));
+		assertEquals("Dcl", p.LHS("Dcl intdcl id"));
+		assertEquals("intdcl id", p.RHS("Dcl intdcl id"));
+	}
+	
 	
 	@Test
 	public void testGrammar() throws FileNotFoundException, IOException
 	{
-		Parser p = new Parser(new Scanner(fileName));
-		ArrayList<String> grammar = p.grammar();
+		Parser p = new Parser(new Scanner(inputFileName));
+		ArrayList<String> grammar = p.getGrammar();
+		int i = 0;
 		
-		assertEquals("Prog Dcls Stms eof", grammar.get(0));
-		fail("da finire");
-		//ecc...
+		assertEquals("Prog Dcls Stms eof", grammar.get(i++));
+		assertEquals("Dcls Dcl Dcls", grammar.get(i++));
+		assertEquals("Dcls eps", grammar.get(i++));
+		assertEquals("Dcl floatdcl id", grammar.get(i++));
+		assertEquals("Dcl intdcl id", grammar.get(i++));
+		assertEquals("Stms Stm Stms", grammar.get(i++));
+		assertEquals("Stms eps", grammar.get(i++));
+		assertEquals("Stm id assign Val Expr", grammar.get(i++));
+		assertEquals("Stm print id", grammar.get(i++));
+		assertEquals("Expr plus Val Expr", grammar.get(i++));
+		assertEquals("Expr minus Val Expr", grammar.get(i++));
+		assertEquals("Expr eps", grammar.get(i++));
+		assertEquals("Val inum", grammar.get(i++));
+		assertEquals("Val fnum", grammar.get(i++));
+		assertEquals("Val id", grammar.get(i++));
 	}
 	
 	@Test
 	public void testSynt() throws IOException
 	{
-		Parser p = new Parser(new Scanner(fileName));
+		Parser p = new Parser(new Scanner(inputFileName));
 		
 		try
 		{
@@ -60,7 +96,7 @@ public class testParser
 	
 	public void testNoSynt() throws IOException
 	{
-		Parser p = new Parser(new Scanner(fileName));
+		Parser p = new Parser(new Scanner(inputFileName));
 		
 		try
 		{
